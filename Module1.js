630aae6f2ff51fc6f200bdf230e9c9a99246302b
elango <elangoravi7@gmail.com>
2023-06-11 18:32:24 +0530

    basic modules added

---
+++ Module1.js
@@ -0,0 +1,8 @@
+const day = 21;
+const month = 6;
+
+function multiply_two_numbers(a, b) {
+    return a * b;
+}
+
+export {day, month, multiply_two_numbers};
---
+++ Module2.js
@@ -0,0 +1,5 @@
+import {day, month, multiply_two_numbers} from './Module1.js';
+
+alert(day);
+alert(multiply_two_numbers(2,3));
+console.log("Asdf");
